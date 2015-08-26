fcba30a820cde1a0ec57ea37e6c8d9ae28e601e9
unknown <yancey1204@gmail.com>
2015-08-26 09:19:42 +0800

    unable the animation of twinkling stars, change the text of page 1 and

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             if (anchorLink == '1stPage') {
                 // animation for the text
                 $("#text").typed({
-                    strings: ["我们结婚啦"],
+                    strings: ["婚礼请柬"],
                     typeSpeed: 350,
                     backDelay: 1000,
                     loop: false,
@@ -21,17 +21,22 @@ $(document).ready(function () {
             if (anchorLink == '2ndPage') {
                 // animation for the text
                 $("#text1").typed({
-                    strings: ["缘分让我们 相遇，相识<br/>" +
-                    "爱恋让我们 相知，相守<br/>"+
-                    "愿成为童话里彼此的天使<br/>" +
-                    "相信着 幸福和快乐是结局<br/>" +
+                    strings: ["一份意外降临的感情<br/>" +
+                    "一场跨州跨洋的爱恋<br/>" +
+                    "从相遇到相恋到如今步入婚姻<br/>" +
+                    "貌似不可思议，貌似意想不到<br/>" +
+                    "我们也迷茫也怀疑也犹豫过<br/>" +
+                    "但和后来我们打算在一起的决心相比<br/>" +
+                    "那些貌似的不可能都只是浮云<br/>" +
+                    "梁思成问林徽因：'为什么是我？'<br/>"+
+                    "林徽因答：'答案很长，我得用一生去回答你'<br/>"+
                     "<br/>" +
                     "新郎： 徐海明<br/>" +
-                    "新娘： 严智慧<br/>"+
-                    "<br/><br/>"+
-                    "希望您能见证这份感情<br>"+
-                    "分享这份甜蜜!"],
-                    typeSpeed: 150,
+                    "新娘： 严智慧<br/>" +
+                    "<br/><br/>" +
+                    "宴席很短<br>" +
+                    "祝福却能长留心中"],
+                    typeSpeed: 100,
                     backDelay: 1000,
                     loop: false,
                     showCursor: false,
@@ -41,9 +46,11 @@ $(document).ready(function () {
             //using anchorLink
             if (anchorLink == '3rdPage') {
                 $("#text2").typed({
-                    strings: ["时间：2015年9月5号， 周六^500<br/>天津惠中酒店^500<br/>"
-                    + "地址：天津市 滨海新区 第三大街3号芳林泰达园A座<br/>"
-                    + "交通：如果您乘坐公交，可在到达塘沽站之后在 赵家地 站上车，" +
+                    strings: ["时间：2015年9月5号， 周六^500<br/>天津惠中酒店^500<br/>"+
+                    "地址：<a href='http://ditu.amap.com/search?query=%E5%A4%A9%E6%B4%A5%E5%B8%82+"+
+                    "%E6%BB%A8%E6%B5%B7%E6%96%B0%E5%8C%BA+"+
+                    "%E5%A4%A9%E6%B4%A5%E6%83%A0%E4%B8%AD%E9%85%92%E5%BA%97&city=120000' target='_blank'>天津市 滨海新区 第三大街3号芳林泰达园A座<br/>"+
+                    "交通：如果您乘坐公交，可在到达塘沽站之后在 赵家地 站上车，" +
                     "518路内环(轻轨东海路-轻轨东海路) 至 南海路站下车，请不要将贵重物品遗忘在车上哦。"],
                     typeSpeed: 80,
                     backDelay: 1000,
@@ -83,15 +90,15 @@ $(document).ready(function () {
         });
 
     // animation of the twinkling star
-    $('.twinkling').velocity(
-        {
-            backgroundPositionY: ["-500px", "1000px"],
-            backgroundPositionX: ["-500px", "1000px"]
-        },
-        {
-            duration: 32500,
-            loop: true
-        });
+    //$('.twinkling').velocity(
+    //    {
+    //        backgroundPositionY: ["-500px", "1000px"],
+    //        backgroundPositionX: ["-500px", "1000px"]
+    //    },
+    //    {
+    //        duration: 32500,
+    //        loop: true
+    //    });
 //animation for the flower
     $(window).load(function () {
         $('body').sakura();
@@ -110,57 +117,16 @@ $(document).ready(function () {
             opacity: 1
         }, 1000);
     });
-    // add a map
-    var map = new AMap.Map('mapContainer', {
-        resizeEnable: true,
-        rotateEnable: true,
-        dragEnable: true,
-        zoomEnable: true,
-        //设置可缩放的级别
-        zooms: [3, 18],
-        //传入2D视图，设置中心点和缩放级别
-        view: new AMap.View2D({
-            center: new AMap.LngLat(117.71801, 39.025124),
-            zoom: 16
-        })
-    });
-
-    addMarker(map);
-    function addMarker(map) {
-        map.clearMap();
-        new AMap.Marker({
-            map: map,
-            icon: 'imgs/map-marker.png',
-            position: new AMap.LngLat(117.71801, 39.025124),
-            offset: new AMap.Pixel(-12, -36)
-        });
-    };
-    map.plugin(["AMap.ToolBar"], function(){
-			//加载工具条
-			var tool = new AMap.ToolBar();
-			map.addControl(tool);
-		});
-
-    // prevent page scrolling when you move the mapContainer on touch device
-    mapContainer.addEventListener('touchstart', function (e) {
-        e.preventDefault(); // prevent default click behavior
-        $.fn.fullpage.setAllowScrolling(false);
-    }, false);
-    // enable page scrolling when you move other parts
-    mapContainer.addEventListener('touchend', function (e) {
-        e.preventDefault(); // prevent default click behavior
-        $.fn.fullpage.setAllowScrolling(true);
-    });
 
     //animation for the button
-    document.getElementById('button').onclick = (function () {
-        document.getElementsByTagName('span')[0].innerHTML = '期待您的光临!';
-        return false;
-    });
-    document.getElementById('button2').onclick = (function () {
-        document.getElementsByTagName('span')[1].innerHTML = '谢谢，祝您生活愉快！';
-        return false;
-    });
+    //document.getElementById('button').onclick = (function () {
+    //    document.getElementsByTagName('span')[0].innerHTML = '期待您的光临!';
+    //    return false;
+    //});
+    //document.getElementById('button2').onclick = (function () {
+    //    document.getElementsByTagName('span')[1].innerHTML = '谢谢，祝您生活愉快！';
+    //    return false;
+    //});
 
     //play the audio
     var vid = document.getElementById("player");
